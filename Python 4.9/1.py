3fb24b81957be13fa6c7878a4704c6c18491202b
Pistipisti94 <112941646+Pistipisti94@users.noreply.github.com>
2022-11-15 09:26:38 +0100

    kész

--- Python 4.9/1.py
+++ Python 4.9/1.py
@@ -5,39 +5,27 @@ screen.bgcolor('lightgreen')
 teki.color('hotpink')
 teki.pensize('3')
 
-for i in range(4):
-    teki.forward(20)
-    teki.left(90)
+class kockak:
+    def loop(ennyi):
+        for i in range(1):
+            teki.forward(20)
+            teki.left(90)
+            teki.forward(20)
+            teki.left(90)
+            teki.forward(20)
+            teki.left(90)
+            teki.forward(20)
+            teki.left(90)
 
-teki.penup()
-teki.forward(40)
-teki.pendown()
+    def loop2(annyi):
+        for i in range(1):
+            teki.penup()
+            teki.forward(40)
+            teki.pendown()
 
-for i in range(4):
-    teki.forward(20)
-    teki.left(90)
-
-teki.penup()
-teki.forward(40)
-teki.pendown()
-
-for i in range(4):
-    teki.forward(20)
-    teki.left(90)
-
-teki.penup()
-teki.forward(40)
-teki.pendown()
-
-for i in range(4):
-    teki.forward(20)
-    teki.left(90)
-
-teki.penup()
-teki.forward(40)
-teki.pendown()
-
-
+for i in range(5):
+    kockak.loop(1)
+    kockak.loop2(1)
 
 
 screen.mainloop()
