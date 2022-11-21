c202731663e3a7a92630816df968e0ed64c85b11
Pistipisti94 <112941646+Pistipisti94@users.noreply.github.com>
2022-11-21 16:15:43 +0100

    .

--- Python 3.8/3.py
+++ Python 3.8/3.py
@@ -1,4 +1,4 @@
-honapok = ["Január", "Február", "Március", "Április", "Május", "Június", "Július", "Augusztus", "Szeptember", "Október", "November", "December"]
+hó = ["Január", "Február", "Március", "Április", "Május", "Június", "Július", "Augusztus", "Szeptember", "Október", "November", "December"]
 
-for honap in honapok:
-   print(f"Az éve egyik hónapja {honap}")
+for honap in hó:
+   print(f"Egy hónap: {honap}")
--- Python 3.8/6.py
+++ Python 3.8/6.py
@@ -3,16 +3,16 @@ import turtle
 screen = turtle.Screen()
 teknos = turtle.Turtle()
 
-def Kiiras(hanyszor, fok):
+def Ki(hanyszor, fok):
    screen.clear()
    teknos = turtle.Turtle()
    for i in range(0, hanyszor):
       teknos.left(fok)
       teknos.forward(100)
 
-Kiiras(3,120)
-Kiiras(4,90)
-Kiiras(6,60)
-Kiiras(8,45)
+Ki(3,120)
+Ki(4,90)
+Ki(6,60)
+Ki(8,45)
 
 screen.mainloop()
