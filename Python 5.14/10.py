393a38e18610013ba6aeb4b71d4a2cdef6707fc2
Pistipisti94 <112941646+Pistipisti94@users.noreply.github.com>
2022-11-22 15:50:15 +0100

    kész

--- Python 5.14/10.py
+++ Python 5.14/10.py
@@ -1,3 +1,15 @@
 #Írj egyatfogofüggvényt, amely megkapja egy derékszög  ̋u háromszög két befogójának a hosszát és visszaadjaaz átfogó hosszát! (Segítség: azx**0.5a négyzetgyököt adja vissza.)
-def atfogo(egy,ketto):
-    
+
+egy = 0
+ketto = 0
+
+isDone = True
+while isDone:
+   try:
+      egy = int(input("Írja be az első befogót hosszát (cm): "))
+      ketto = int(input("Írja be a második befogót hosszát (cm): "))
+      isDone = False
+   except:
+      print("Rossz adatbevitel!")
+
+print(f"A derkészögű háromszög átfogója {(((egy*egy) + (ketto*ketto)) ** 0.5)}cm.")
