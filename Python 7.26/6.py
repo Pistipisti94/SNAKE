992d7071e6f0ff2df4bfbee66f332afb5cb675c3
Pistipisti94 <112941646+Pistipisti94@users.noreply.github.com>
2022-12-13 08:50:30 +0100

    kesz

--- Python 7.26/6.py
+++ Python 7.26/6.py
@@ -4,8 +4,20 @@ def szonez():
     for x in t:
         if x !="nem":
             a = a + 1
-        elif x == "nem":
+        else:
             a = a + 1
-            
-    print(a)
-szonez()
+            return a
+    
+print(szonez())
+
+def szonez2():
+    t = ["asd","ferike","igen","asd","ferike","igen"]
+    a = 0
+    for x in t:
+        if x !="nem":
+            a = a + 1
+        else:
+            a = a + 1
+            return a
+    
+print(szonez2())
