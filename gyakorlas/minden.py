f5afaffd3213df12f620f32991b31dd841821f5c
Pistipisti94 <112941646+Pistipisti94@users.noreply.github.com>
2022-12-15 11:43:14 +0100

    jolvaaan

---
+++ gyakorlas/minden.py
@@ -0,0 +1,14 @@
+from osztaly import *
+
+t1= teglalap()
+print("t1 kerület: {0}, területe: {1}".format(t1.getkerulet(),t1.getterulet()))
+
+t1.setA(12)
+t1.setB(56)
+print(f"A t1 kerülete:: {t1.getkerulet()},területe: {t1.getterulet()}")
+
+t1= negyzet()
+print("t1 kerület: {0}, területe: {1}".format(t1.getkerulet(),t1.getterulet()))
+
+t1.setA(12)
+print(f"A t1 kerülete:: {t1.getkerulet()},területe: {t1.getterulet()}")
--- gyakorlas/teglalap.py
+++ gyakorlas/teglalap.py
@@ -6,3 +6,4 @@ print("t1 kerület: {0}, területe: {1}".format(t1.getkerulet(),t1.getterulet())
 t1.setA(12)
 t1.setB(56)
 print(f"A t1 kerülete:: {t1.getkerulet()},területe: {t1.getterulet()}")
+
