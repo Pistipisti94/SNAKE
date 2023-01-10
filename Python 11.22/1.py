27ac24daee29836148e8376fe5866e6e40e0fd35
Pistipisti94 <112941646+Pistipisti94@users.noreply.github.com>
2023-01-10 08:18:58 +0100

    kesz

---
+++ Python 11.22/1.py
@@ -0,0 +1,4 @@
+#Ha a kiindulási pont nagyobb a végpontnál nincs probléma ha a léptetési érték < 0
+#Ha nincs léptetési érték a kiíratott érték egy üres lista lesz
+list = list(range(10, 0, -2))
+print(list)
---
+++ Python 11.22/2.py
@@ -0,0 +1,9 @@
+import turtle
+
+Eszti = turtle.Turtle()
+Sanyi = Eszti
+Sanyi.color("hotpink")
+win = turtle.Screen()
+#Ez a kód egy teknőst hoz létre viszont Sanyi nevű változóval is be lehet hívni Esztit.
+#Sanyi színének megváltoztatásával Eszti színe változik
+win.mainloop()
