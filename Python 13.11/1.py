8972a907d7c83e0be4cc2b12d4027dc4ccc44ce4
Pistipisti94 <112941646+Pistipisti94@users.noreply.github.com>
2023-01-16 17:40:53 +0100

    ahhh kesz, 4 es nem

--- Python 13.11/1.py
+++ Python 13.11/1.py
@@ -1,7 +1,16 @@
-fajl = open("Python 13.11\\fajl","r")
-fajl.readlines()
-fajl.
-fajl.close()
-ujfajl = open("Python 13.11\\ujfajl","w")
+lista = []
+file = open("Python 13.11\\fajl", "r")
+for line in file:
+   lista.append(line[0:len(line) - 1])
+file.close()
 
-ujfajl.close()
+index = len(lista) - 1
+forditott_lista = []
+for l in lista:
+   forditott_lista.append(lista[index])
+   index = index - 1
+
+file = open("Python 13.11\\ujfajl", "w")
+for x in forditott_lista:
+   file.writelines(f"{x}\n")
+file.close()
